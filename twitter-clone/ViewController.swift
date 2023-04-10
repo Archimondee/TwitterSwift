e707b76a0ce5a93b51fb1099d575bc8a3ff58f52
Archimondee <kikchiemedia@gmail.com>
2023-04-11 00:02:44 +0700

    Initial Commit

---
+++ twitter-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter-clone
+//
+//  Created by Gilang Aditya Rahman on 11/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
